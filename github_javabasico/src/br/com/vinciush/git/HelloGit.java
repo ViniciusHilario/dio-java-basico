c86512e4b9979afbae743bcbca403183aef80931
Vinicius Hilario <69550939+ViniciusHilario@users.noreply.github.com>
2025-09-09 21:46:55 -0300

    Adicionei via upload

---
+++ github_javabasico/src/br/com/vinciush/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.vinciush.git;
+
+/**
+ * 
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 * Exemplo de integração com o git
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Git");
+	}
+
+}
